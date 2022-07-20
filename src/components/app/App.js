5a5eedca15bbaf497b94c2914e425ebb533100c7
dorcas-1995 <wayumulu58@gmail.com>
2022-07-20 06:26:14 -0400

    assignment commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import Navbar from '../navbar/Navbar';
 import Contact from '../contact/Contact';
 import About from '../about/About';
 import Home from '../home/Home';
+import {BrowserRouter as Router,Routes,link } from 'react-router-dom'
 
 function App() {
   return (
@@ -15,6 +16,40 @@ function App() {
   );
 }
 
+function MainApp(){
+
+  return(
+    <Router>
+      <div>
+    <div className="topNavBar">
+            <link className="active" to="/">Home</link>.
+            <link to="/contact">Contact</link>
+            <link to="/about">About</link>
+        </div>
+        
+  <Routes>
+
+ 
+    <Route exact path='/' element={<Home/>}/>
+    <Route exact path='/contact' element={<Contact/>}/>
+    <Route exact path='/about' element={<About/>}/>
+    <Route exact path='user/:username' element={<User/>}/>
+    
+    </Routes>
+  </div>
+
+1</Router>
+
+
+    
+
+  );
+    
+  }
+
+
+
 
 
 export default App;
+export {MainApp}
